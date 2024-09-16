8f2284b08f5ffcf75b929f022d96f24808af64e3
nicoleabrand <nicoleabrand13@gmail.com>
2024-09-16 12:38:32 -0300

    feat: rotas #12

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import './assets/main.css'
 </script>
 <template>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">Cadastro</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
   <RouterView />
-  <HelloWorld></HelloWorld>
 </template>
 
 <style scoped>
