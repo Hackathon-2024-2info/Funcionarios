62f8d34aaa0b2a582c1cd3ad24be3944f36af86a
melloyasminn <melloyasminm@gmail.com>
2024-09-23 15:10:15 -0300

    feat #8

--- src/components/MenuFuncionarios.vue
+++ src/components/MenuFuncionarios.vue
@@ -5,27 +5,86 @@
 <div class="menu">
     <nav>
         <img src="../assets/logo AVANTE.png" alt="" class="img">
+        <button class="btn-branco">Estoque</button>
+        <button class="btn-azul">Serviço</button>
+        <button class="btn-branco">Relatório</button>
+    <img src="../assets/Question.png" alt="" class="warning">
+        <div class="box">
+        <p class="title">Abra um chamado</p>
+        <p>Dúvidas ou problemas, abra um chamado ao administrador.</p>
+        <button>Abrir chamado</button>
+        </div>
     </nav>
 </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Kantumruy+Pro:ital,wght@0,100..700;1,100..700&display=swap');
 .menu{
     display: grid;
-    grid-template-columns: 15% 1fr;
+    grid-template-columns: 17% 1fr;
 }
 nav{
     background: #0F0541;
     height: 100vh;
     width: 100%;
+
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    
+    justify-content: start;
+    align-items: center;
 }
 
 .img{
-    width: 120px;
-    height: 120px;
+    width: 14vh;
+    height: 14vh;
     margin-top: 10%;
+  align-items: center;
 }
 
+.btn-azul{
+    font-family: "Plus Jakarta Sans", sans-serif;
+    background-color: #384DBB;
+    border-radius: 10px;
+    border: none;
+    width: 24vh;
+    height: 6%;
+    color: white;
+
+    font-size: 14px;
+    font-weight: 600;
+    margin: 2vh;
+
+}
+
+.btn-branco{
+    font-family: "Plus Jakarta Sans", sans-serif;
+    background-color: white;
+    border: none;
+    border-radius: 20px;
+    width: 20vh;
+    height:  5%;
+    font-size: 14px;
+    font-weight: 600;
+    margin: 4vh;
+} 
+
+.box{
+    background-color: #384DBB;
+    color: white;
+    font-family: "Kantumruy Pro", sans-serif;
+    font-weight: 400;
+    border-radius: 10px;
+    margin-left: 15%;
+    margin-right: 15% ;
+    height: 30%;
+    padding-top: 30%;
+}
+
+.title{
+    font-size: 128%;
+    text-align: center;
+}
 </style>
