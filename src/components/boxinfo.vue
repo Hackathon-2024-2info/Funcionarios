f615669aef80d500cf2d265d50f085de205c7bc6
eduardafh <eduardafheidemann@gmail.com>
2024-09-23 09:23:30 -0300

    feat: inicio css #15

--- src/components/boxinfo.vue
+++ src/components/boxinfo.vue
@@ -3,12 +3,81 @@
 
 <template>
 
-<div class="box"></div>
-
+    <body>
+        <div class="box">
+            <img src="../assets/Task.png" alt="mapaDps">
+            <p class="titulo">MANUTENÇÃO PREVENTIVA</p>
+            <p class="minicontainer"> Cliente: Krona</p>
+            <div class="infos">
+                <p>Prestadores: Edson e Fulano</p>
+                <p>Previsão de duração: 2 dias</p>
+                <p>Contato do responsável pela máquina: (47) 99012-9302</p>
+                <p>Descrição: Manutenção de máquina injetora tipo xxxxxxxxxx lalalalaal cucucucu suqwbuqhuie3uiwri32
+                    relatados
+                    ruídos na máquina</p>
+            </div>
+        </div>
+    </body>
 </template>
 
+
 <style scoped>
-.box{
-    
+@import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Readex+Pro:wght@160..700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap');
+
+.box {
+    position: absolute;
+    width: 696px;
+    height: 744px;
+    left: 332px;
+    top: 178px;
+    background: #FFFFFF;
+    border-radius: 10px;
+
+}
+
+.titulo {
+    /* MANUTENÇÃO PREVENTIVA */
+    position: absolute;
+    width: 455px;
+    height: 38px;
+    left: 3%;
+    top: 51%;
+    font-family: 'Kantumruy';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 120%;
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    color: #091D87;
+}
+
+.minicontainer {
+    position: absolute;
+    width: 30%;
+    height: -8%;
+    left: 3%;
+    top: 59%;
+    font-family: 'Readex Pro';
+    background: rgba(56, 77, 187, 0.4);
+    border-radius: 20px;
+    text-align: center;
+    padding-top: 1%;
+}
+
+.infos{
+font-family: 'Karla';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 200%;
+padding-top: 18%;
+margin-left: 3%;
+}
+.info p{
+color: black;
 }
 </style>
