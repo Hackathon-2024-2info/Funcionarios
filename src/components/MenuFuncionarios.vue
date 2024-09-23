e29bef201cba7a35f438c577a6d89925c216cdb5
melloyasminn <melloyasminm@gmail.com->
2024-09-23 09:23:46 -0300

    feat: início do menu #8

---
+++ src/components/MenuFuncionarios.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+<div class="menu">
+    <nav>
+        <img src="../assets/logo AVANTE.png" alt="" class="img">
+    </nav>
+</div>
+</template>
+
+<style scoped>
+.menu{
+    display: grid;
+    grid-template-columns: 15% 1fr;
+}
+nav{
+    background: #0F0541;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+.img{
+    width: 120px;
+    height: 120px;
+    margin-top: 10%;
+}
+
+</style>
--- src/views/ServicosView.vue
+++ src/views/ServicosView.vue
@@ -1,7 +1,11 @@
 <script setup>
 import boxinfo from "../components/boxinfo.vue";
+import MenuFuncionarios from "../components/MenuFuncionarios.vue";
+
 </script>
 
 <template> 
+<MenuFuncionarios></MenuFuncionarios>
 <boxinfo></boxinfo>
+
 </template>
