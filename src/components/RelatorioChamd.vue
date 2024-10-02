95c4ba85bf78ae8e7d9b4da76930fbe41f7be3ea
nathyadao <nathalycarolineadaoo@gmail.com>
2024-10-02 13:43:03 -0300

    feat: começando relatorio chamado #26

---
+++ src/components/RelatorioChamd.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <main id="main">
+    <div>
+      
+
+      <h1 class="solicitar"></h1>
+      <label for=""> Motivo </label>
+      <input class="input1" placeholder="Insira o motivo do chamado">
+
+    </div>
+    <div class="espacamento"></div>
+    <div>
+      <label for=""> Descrição </label>
+      <input class="input2" placeholder="Insira a decriçâo">
+      <button id="enviar" type="submit">Enviar</button>
+    </div>
+
+  </main>
+</template>
