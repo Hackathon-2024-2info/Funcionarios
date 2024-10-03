86cb4c1decc54be305418f94cea5b7181ad94c4c
eduardafh <eduardafheidemann@gmail.com>
2024-10-03 20:51:21 -0300

    feat: inicio desenvolvimento #19

---
+++ src/components/relPreventivo.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <div class="row">
+        <label for="">Senha: </label>
+        <input type="checkbox" placeholder="Digite uma senha">
+      </div>
+    </div>
+</template>
+
+<style scoped></style>
