91a3670f3cc19db6d59782581211cd1dd0b95a01
eduardafh <eduardafheidemann@gmail.com>
2024-10-04 09:23:51 -0300

    feat: inicio #19

--- src/components/relPreventivo.vue
+++ src/components/relPreventivo.vue
@@ -4,10 +4,47 @@
 <template>
     <div>
         <div class="row">
-        <label for="">Senha: </label>
-        <input type="checkbox" placeholder="Digite uma senha">
+        <label for="">Limpeza e organização dos equipamentos: </label>
+        <input type="checkbox">
+        <input type="checkbox">
+      </div>
+      <div class="row">
+        <label for="">Testes dos movimentos (injeção, dosagem, fechamento, extrator): </label>
+        <input type="checkbox">
+        <input type="checkbox">
+      </div>
+      <div class="row">
+        <label for="">Funcionamento da segurança (NR12):</label>
+        <input type="checkbox">
+        <input type="checkbox">
+      </div>
+      <div class="row">
+        <label for="">Funcionamento do manipulador:</label>
+        <input type="checkbox">
+        <input type="checkbox">
+      </div>
+      <div class="row">
+        <label for="">Funcionamento do alimentador:</label>
+        <input type="checkbox">
+        <input type="checkbox">
+      </div>
+      <div class="row">
+        <label for="">Existe ruído anormal no aparelho?:</label>
+        <input type="checkbox">
+        <input type="checkbox">
+      </div>
+      <div class="row">
+        <label for="">Aparelho em condições seguras para operar:</label>
+        <input type="checkbox">
+        <input type="checkbox">
+      </div>
+      <div class="row">
+        <label for="">Aquecimento conforme programado:</label>
+        <input type="checkbox">
+        <input type="checkbox">
       </div>
     </div>
+    <button>Próximo</button>
 </template>
 
 <style scoped></style>
