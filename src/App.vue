d5addc94dbc140c9c3645653d6d5605c9b3cf5ed
manuhostin <hostimmanu@gmail.com>
2024-10-11 14:57:36 -0300

    feat: mapa mostrando #5

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
 <template>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-  <RouterView />
-  <HelloWorld></HelloWorld>
-</template>
-
-<style scoped>
-
-</style>
+  <div ref="map" style="width:500px; height: 500px;"></div>
+ </template>
+ <script setup>
+  import { ref, onMounted } from 'vue';
+  const map = ref(null);
+ 
+ 
+  onMounted(()=> {
+    new window.google.maps.Map(map.value, {
+      center: {lat: 45.4215296, lng: -75.6971931 },
+      zoom: 10
+    });
+ 
+ 
+  }
+   )
+ </script>
