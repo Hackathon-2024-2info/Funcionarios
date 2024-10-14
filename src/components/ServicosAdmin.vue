97e160f69bf9cb141a01d3904f4954665625b8a7
melloyasminn <melloyasminm@gmail.com>
2024-10-14 15:06:15 -0300

    feat: inicio d pag de servicos de admin #33

---
+++ src/components/ServicosAdmin.vue
@@ -0,0 +1,99 @@
+<script setup>
+</script>
+
+<template>
+    <main>
+    <div class="filtrar">
+      <button class="btn-azul"> ADICIONAR NOVO SERVIÇO</button>
+      <label for="">Filtrar:</label>
+      <select name="Selecione" id="Selecione">
+        <option value="" class="option">SELECIONE</option>
+      </select>
+    </div>
+    <div class="setas">
+      <button><img src="../assets/esquerda.png" alt="" /></button>
+      <p>1</p>
+      <button><img src="../assets/direita.png" alt="" /></button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+section{ 
+    display: grid   ;
+    grid-template-columns: 50% 50%;
+}
+
+label {
+  margin-right: 3%;
+}
+label,
+select,
+option {
+  font-family: 'Karla';
+}
+
+main {
+  margin-top: 5%;
+}
+p {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  font-style: normal;
+  margin-left: 2%;
+  margin-right: 2%;
+  background: rgb(84, 111, 255, 0.32);
+  padding: 1.9%;
+  border-radius: 10px;
+}
+body{
+  color: #F5F5F5;
+}
+.filtrar {
+  flex-wrap: nowrap;
+  width: 100%;
+}
+button {
+  border: none;
+  background: white;
+}
+
+select {
+  border-radius: 20px;
+  border: none;
+  background: rgb(84, 111, 255, 0.32);
+  color: #8787c7;
+  padding: 0.2%;
+  padding-left: 2%;
+  padding-right: 2%;
+}
+.btn-azul {
+  border: none;
+  border-radius: 5px;
+  color: white;
+  font-family: 'Karla';
+  background-color: #091d87;
+  padding: 1.5%;
+  padding-left: 5%;
+  padding-right: 5%;
+  font-weight: 700;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+
+main {
+  display: flex;
+  margin-bottom: 3%;
+}
+
+.setas {
+  width: 100%;
+  display: flex;
+  flex-wrap: nowrap;
+  margin-left: -5%;
+  margin-top: -0.4%;
+}
+</style>
---
+++ src/views/ServicosAdminView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import EstoqueAdmin from '@/components/ServicosAdmin.vue';
+</script>
+
+<template>
+   <EstoqueAdmin></EstoqueAdmin>
+</template>
