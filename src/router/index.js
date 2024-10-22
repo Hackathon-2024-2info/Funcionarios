bc1adc0fbc951078d3d8c9bca913b921a39d6815
eduardafh <eduardafheidemann@gmail.com>
2024-10-22 20:12:52 -0300

    feat: rota arrumada. finalizado #19

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,14 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      component: () => import('@/views/AboutView.vue')
+    },
+    {
+      path: '/relpreventivo',
+      name: 'relpreventivo',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
       component: () => import('@/components/relPreventivo.vue')
     }
   ]
