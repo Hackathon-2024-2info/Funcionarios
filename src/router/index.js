7847184dd0fa089aec00f96808f38c9bcceb1490
nicoleabrand <nicoleabrand13@gmail.com>
2024-10-25 16:23:35 -0300

    feat: inicio #75

--- src/router/index.js
+++ src/router/index.js
@@ -23,14 +23,6 @@ const router = createRouter({
       component: () => import('../views/SolicitarChamadoView.vue')
     },   
     {
-      path: '/relatoriochamado',
-      name: 'relatoriochamado',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/components/RelatorioChamd.vue')
-    },
-    {
       path: '/editperfil',
       name: 'editperfil',
       // route level code-splitting
--- src/views/RelatoriosFuncionariosView.vue
+++ src/views/RelatoriosFuncionariosView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import MenuFuncionarios from "@/components/MenuFuncionarios.vue";
 import pagHeader from "@/components/pagHeader.vue";
+import RelatorioFuncionarios from "@/components/RelatorioFuncionarios.vue";
 </script>
 
 <template> 
 <pagHeader></pagHeader>
 <MenuFuncionarios></MenuFuncionarios>
+<RelatorioFuncionarios></RelatorioFuncionarios>
 </template>
