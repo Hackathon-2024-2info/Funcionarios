b847a9ec535e87756442ae868455002519b5e85e
eduardafh <eduardafheidemann@gmail.com>
2024-10-24 22:04:11 -0300

    feat: inicio desenvolvimento em mobile #66

--- src/components/pagPerfil.vue
+++ src/components/pagPerfil.vue
@@ -22,19 +22,32 @@
 @import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Readex+Pro:wght@160..700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap');
 
-
-
-.infos{
-    display: flex;
-    flex-direction: column; /* Organiza os itens em uma linha */
-    width: 30%;
-    margin-left: 32%;
-    align-items: center; /* Garante que os itens fiquem alinhados no centro verticalmente */
-    flex-wrap: wrap; /* Faz com que os itens "quebrem linha" em telas menores */
+header {
+    position: absolute;
+    background-color: #0F0541;
+    height: 50%;
+    width: 100%;
+    margin-top: 0;
 }
+.foto-perfil {
+    position: relative;
+    margin-top: 35%; 
+    margin-left: 47%;
+    transform: translateX(-45%); 
+    }
+    .infos{
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    margin-left: 18%;
+    align-items: center;
+    flex-wrap: wrap; 
+    margin-top: 7%;
+}
+
 p {
     margin: 0;
-    white-space: nowrap; /* Evita quebras de linha dentro do parágrafo */
+    white-space: nowrap;
 }
 
 
@@ -42,7 +55,7 @@ p {
     font-family: 'Readex Pro';
     font-style: normal;
     font-weight: 600;
-    font-size: 32px;
+    font-size: 24px;
     display: flex;
     color: #000000;
 }
@@ -52,9 +65,9 @@ p {
     font-family: 'Karla';
     font-style: normal;
     font-weight: 300;
-    font-size: 20px;
+    font-size: 18px;
     display: flex;
-    margin-top: 2%;
+    margin-top: 7%;
     color: #000000;
 }
 
@@ -63,7 +76,7 @@ p {
     font-family: 'Karla';
     font-style: normal;
     font-weight: 300;
-    font-size: 20px;
+    font-size: 18px;
     display: flex;
     margin-top: 0.5%;
     color: #000000;
@@ -73,26 +86,26 @@ p {
     font-family: 'Karla';
     font-style: normal;
     font-weight: 300;
-    font-size: 20px;
+    font-size: 18px;
     display: flex;
     color: #000000;
-    margin-top: 30%;
+    margin-top: 60%;
 }
 
 .num {
     font-family: 'Karla';
     font-style: normal;
     font-weight: 300;
-    font-size: 20px;
-    margin-top: 2%;
+    font-size: 18px;
+    margin-top: 30%;
     display: flex;
     color: #000000;
 }
 
 button {
     display: flex;
-    width: 60%;
-    height: 6vh;
+    width: 200%;
+    padding: 5%;
     justify-content: center;
     text-align: center;
     align-items: center;
@@ -100,45 +113,8 @@ button {
     border-radius: 5px;
     color: white;
     border: none;
-    margin-top: 20%;
-    margin-bottom: 5%;
-    margin-left: 1%;
+    margin-top: 50%;
+    margin-bottom: 10%;
+    margin-left: 2%;
 }
-
-@media only screen and (max-device-width: 1500px) {
-    
-    header {
-    position: absolute;
-    background-color: #0F0541;
-    height: 30%;
-    width: 100%;
-    margin-top: 0;
-}
-
-.foto-perfil {
-    position: relative;
-    margin-top: 5%; /* Ajusta a imagem para ficar "meio a meio" */
-    margin-left: 47%; /* Centraliza horizontalmente */
-    transform: translateX(-50%); /* Centraliza de forma perfeita */
-}
-    }
-    
-    @media only screen and (max-device-width: 2000px) {
-        
-        header {
-    position: absolute;
-    background-color: #0F0541;
-    height: 30%;
-    width: 100%;
-    margin-top: 0;
-}
-
-.foto-perfil {
-    position: relative;
-    margin-top: 8.5%; /* Ajusta a imagem para ficar "meio a meio" */
-    margin-left: 47%; /* Centraliza horizontalmente */
-    transform: translateX(-50%); /* Centraliza de forma perfeita */
-}
-        }
-        
 </style>
