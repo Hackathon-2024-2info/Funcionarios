ea772c0a434062a0284d52ede61f22cc668b9520
eduardafh <eduardafheidemann@gmail.com>
2024-10-24 22:04:11 -0300

    feat: inicio desenvolvimento em mobile #66

--- src/components/PagLogin.vue
+++ src/components/PagLogin.vue
@@ -42,7 +42,7 @@ function processarForm() {
       
         <h1>SEJA BEM-VINDO!</h1>
         <p class="pergunta">Ainda não tem uma conta?</p>
-        <RouterLink to="/about"><button id="enviar" type="submit">Cadastre-se</button></RouterLink>
+        <RouterLink to="/perfil"><button id="enviar" type="submit">Cadastre-se</button></RouterLink>
      
     </section>
 
