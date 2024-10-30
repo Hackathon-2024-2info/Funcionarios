082711639620c7dedd402a1bfac2ac6367f24da0
nathyadao <nathalycarolineadaoo@gmail.com>
2024-10-30 15:08:20 -0300

    feat: ajustes concluidos #75

--- src/components/RelatorioFuncionarios.vue
+++ src/components/RelatorioFuncionarios.vue
@@ -61,22 +61,22 @@
   <div class="container">
     <label for="">Descrição da máquina e da realização do serviço</label>
     <input class="input3" placeholder="Insira a descrição">
-  <div class="espacing" @click="openFileSelection">
-    <fieldset class="arquivo">
-      <div class="archive-input">
-        <input type="file" @change="handleFileChange" accept=".zip,.tar,.tar.gz, .pdf, .txt" ref="fileInput"
-          style="display: none" />
-        <button @click="triggerFileInput" class="icon-button">
-          <i class="fas fa-file-archive"> <img src="@/assets/image-add.png" alt="" id="imagelogo"></i>
-          <!-- Example icon -->
-        </button>
-        <div id="arquive-selected" v-if="fileName">Arquivo Selecionado: {{ fileName }}</div>
-      </div>
-      <h6>INSIRA AQUI</h6>
-      <h4>Imagens da máquina DEPOIS do serviço</h4>
-    </fieldset>
-  </div> 
-   </div>
+    <div class="espacing" @click="openFileSelection">
+      <fieldset class="arquivo">
+        <div class="archive-input">
+          <input type="file" @change="handleFileChange" accept=".zip,.tar,.tar.gz, .pdf, .txt" ref="fileInput"
+            style="display: none" />
+          <button @click="triggerFileInput" class="icon-button">
+            <i class="fas fa-file-archive"> <img src="@/assets/image-add.png" alt="" id="imagelogo"></i>
+            <!-- Example icon -->
+          </button>
+          <div id="arquive-selected" v-if="fileName">Arquivo Selecionado: {{ fileName }}</div>
+        </div>
+        <h6>INSIRA AQUI</h6>
+        <h4>Imagens da máquina DEPOIS do serviço</h4>
+      </fieldset>
+    </div>
+  </div>
 
   <div>
     <button id="enviar" type="submit">Enviar</button>
@@ -143,14 +143,15 @@
   width: 99%;
   top: 59%;
 }
+
 .espacamento {
   margin-top: 2.8%;
 }
 
 
 #opcoes {
-  width: 465px;
-  height: 53px;
+  width: 472px;
+  height: 62px;
   background: rgba(217, 217, 217, 0.43);
   border-radius: 5px;
   border: none;
@@ -222,7 +223,6 @@
 }
 
 
-
 label {
   font-family: 'Karla';
   font-style: normal;
@@ -246,7 +246,7 @@ label {
   border-radius: 5px;
   font-family: 'Karla';
   padding: 1% 1%;
-  width: 148px;
+  width: 8%;
   font-weight: 700;
   font-size: 16px;
   color: white;
@@ -276,7 +276,8 @@ label {
   display: grid;
   margin-left: 6%;
   grid-template-rows: 20% 40% 20%;
-  background-color: rgba(217, 217, 217, 0.43);;
+  background-color: rgba(217, 217, 217, 0.43);
+  ;
 }
 
 h4 {
@@ -305,162 +306,14 @@ h6 {
 }
 
 
-.icon-button{
+.icon-button {
   border: 0px;
   margin-top: 2%;
   margin-left: 5%;
 }
 
-.espacing{
+.espacing {
   height: 2vh;
-}
-
-
-#arquive-selected{
-    font-size: 12px;
-    color: #000000;
-    font-family: 'Karla';
-    font-weight: 300;
-    margin-top: 4%;
-    margin-left: 2%;
-}
-
-
-
-#imagelogo {
-    width: 24px;
-    height: 24px;
-    padding: 3%;
-    left: 12%;
-
-}
-
-@media (max-width: 480px) {
-  
-#main {
-  display: grid;
-  align-items: center;
-}
-
-.container {
- position: relative;
- left: 0px;
-}
-
-
-.relatorio {
-  font-family: 'Readex Pro';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 18px;
-  color: #180577;
-  margin-left: -11%;
-  width: 273px;
-}
-
-#opcoes {
-  width: 297px;
-  height: 44px;
-  background: rgba(217, 217, 217, 0.43);
-  border-radius: 5px;
-  border: none;
-  outline: none;
-  font-family: 'Karla';
-  font-style: normal;
-  font-size: 12px;
-  color: #687C94;
-  padding-left: 3%;
-}
-
-
-
-.input1 {
-  padding: 10px 16px;
-  gap: 10px;
-  width: 263px;
-  height: 34px;
-  background: rgba(217, 217, 217, 0.43);
-  border-radius: 5px;
-  border: none;
-  outline: none;
-  font-family: 'Karla';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 14px;
-  color: #687C94;
-  display: grid;
-  align-self: center;
-}
-
-
-
-
-.input2 {
-  padding: 10px 16px;
-  width: 263px;
-  height: 34px;
-  background: rgba(217, 217, 217, 0.43);
-  border-radius: 5px;
-  font-family: 'Karla';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 14px;
-  color: #687C94;
-  border: none;
-  outline: none;
-  display: grid;
-  align-self: center;
-
-}
-
-
-.input3 {
-  width: 263px;
-  height: 100px;
-  background: rgba(217, 217, 217, 0.43);
-  border-radius: 5px;
-  font-family: 'Karla';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 14px;
-  color: #687C94;
-  border: none;
-  outline: none;
-  display: grid;
-  align-self: center;
-}
-
-
-
-label {
-  font-family: 'Karla';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 13px;
-  margin-left: 2%;
-  color: #666666;
-  display: grid;
-  align-self: center;
-}
-
-
-#enviar {
-  background-color: #091D87;
-  border-radius: 5px;
-  font-family: 'Karla';
-  padding: 15px 40px;
-  width: 148px;
-  height: 35px;
-  font-weight: 700;
-  font-size: 16px;
-  color: white;
-  margin-top: 3rem;
-  border: none;
-  text-align: center;
 }
 
 
@@ -470,84 +323,215 @@ label {
   font-family: 'Karla';
   font-weight: 300;
   margin-top: 4%;
-}
-
-.arquivo {
-  width: 63%;
-  height: 6vh;
-  margin-left: -1%;
-  border-radius: 20px;
-  border-color: rgba(217, 217, 217, 0.43);
-  margin-top: 10%;
-  display: grid;
-  margin-left: 6%;
-  grid-template-rows: 20% 40% 20%;
-  background-color: rgba(217, 217, 217, 0.43);;
-}
-
-h4 {
-  font-size: 12px;
-  color: #555555;
-  font-family: 'Karla';
-  font-weight: 300;
-  padding-left: 20%;
-}
-
-h6 {
-  font-family: 'Karla';
-  font-size: 16px;
-  color: #091D87;
-  font-weight: 400;
-  padding-left: 20%;
+  margin-left: 2%;
 }
 
 
 
-#file-input {
-  background-image: ("@/assets/iconimage.png");
-  width: 34px;
-  height: 34px;
+#imagelogo {
+  width: 24px;
+  height: 24px;
+  padding: 3%;
+  left: 12%;
 
 }
 
+@media (max-width: 480px) {
 
-.icon-button{
-  border: 0px;
-  margin-top: 2%;
-}
+  #main {
+    display: grid;
+    align-items: center;
+  }
 
-.espacing{
-  height: 2vh;
-}
+  .container {
+    position: relative;
+    left: 0px;
+  }
 
 
-#arquive-selected{
+  .relatorio {
+    font-family: 'Readex Pro';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 18px;
+    color: #180577;
+    margin-left: -11%;
+    width: 273px;
+  }
+
+  #opcoes {
+    width: 20%;
+    background: rgba(217, 217, 217, 0.43);
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    font-family: 'Karla';
+    font-style: normal;
+    font-size: 12px;
+    color: #687C94;
+    padding-left: 3%;
+  }
+
+
+
+  .input1 {
+   
+    background: rgba(217, 217, 217, 0.43);
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    font-family: 'Karla';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #687C94;
+    display: grid;
+    align-self: center;
+  }
+
+
+
+
+  .input2 {
+    
+    background: rgba(217, 217, 217, 0.43);
+    border-radius: 5px;
+    font-family: 'Karla';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #687C94;
+    border: none;
+    outline: none;
+    display: grid;
+    align-self: center;
+
+  }
+
+
+  .input3 {
+    background: rgba(217, 217, 217, 0.43);
+    border-radius: 5px;
+    font-family: 'Karla';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #687C94;
+    border: none;
+    outline: none;
+    display: grid;
+    align-self: center;
+  }
+
+
+
+  label {
+    font-family: 'Karla';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+    margin-left: 2%;
+    color: #666666;
+    display: grid;
+    align-self: center;
+  }
+
+
+  #enviar {
+    background-color: #091D87;
+    border-radius: 5px;
+    font-family: 'Karla';
+    padding: 15px 40px;
+    width: 148px;
+    height: 35px;
+    font-weight: 700;
+    font-size: 16px;
+    color: white;
+    margin-top: 3rem;
+    border: none;
+    text-align: center;
+  }
+
+
+  #arquive-selected {
+    font-size: 12px;
+    color: #000000;
+    font-family: 'Karla';
+    font-weight: 300;
+    margin-top: 4%;
+  }
+
+  .arquivo {
+    width: 63%;
+    height: 6vh;
+    margin-left: -1%;
+    border-radius: 20px;
+    border-color: rgba(217, 217, 217, 0.43);
+    margin-top: 10%;
+    display: grid;
+    margin-left: 6%;
+    grid-template-rows: 20% 40% 20%;
+    background-color: rgba(217, 217, 217, 0.43);
+    ;
+  }
+
+  h4 {
+    font-size: 12px;
+    color: #555555;
+    font-family: 'Karla';
+    font-weight: 300;
+    padding-left: 20%;
+  }
+
+  h6 {
+    font-family: 'Karla';
+    font-size: 16px;
+    color: #091D87;
+    font-weight: 400;
+    padding-left: 20%;
+  }
+
+
+
+  #file-input {
+    background-image: ("@/assets/iconimage.png");
+    width: 34px;
+    height: 34px;
+
+  }
+
+
+  .icon-button {
+    border: 0px;
+    margin-top: 2%;
+  }
+
+  .espacing {
+    height: 2vh;
+  }
+
+
+  #arquive-selected {
     font-size: 12px;
     color: #000000;
     font-family: 'Karla';
     font-weight: 300;
     margin-top: 4%;
     margin-left: 2px;
-}
+  }
 
 
 
-#imagelogo {
+  #imagelogo {
     width: 24px;
     height: 24px;
     padding: 3%;
     left: 12%;
 
+  }
 }
-}
-
-
-
-
-
-
-
-
-
-
 </style>
