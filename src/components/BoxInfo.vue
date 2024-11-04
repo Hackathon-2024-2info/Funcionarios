bff1c4d7fb4bdd00a0b9eed35fa9c591d959eb11
eduardafh <eduardafheidemann@gmail.com>
2024-11-04 08:14:02 -0300

    feat: inicio router #85

--- src/components/BoxInfo.vue
+++ src/components/BoxInfo.vue
@@ -90,7 +90,6 @@ const itens = [
 .teste{
   display: flex;
   justify-content: end;
-  margin-top: -30%;
   margin-bottom: 30%;
 }
 
--- src/views/AddEstoqueAdministradoresView.vue
+++ src/views/AddEstoqueAdministradoresView.vue
@@ -1,7 +1,12 @@
 <script setup>
  import EstoqueAdministradores from '@/components/AddEstoqueAdministradores.vue';
+ import MenuAdministradores from '@/components/MenuAdministradores.vue';
+ import pagHeader from '@/components/pagHeader.vue';
 
 </script>
 <template>
+        <pagHeader></pagHeader>
+        <MenuAdministradores></MenuAdministradores>
     <EstoqueAdministradores></EstoqueAdministradores>
+
  </template>
