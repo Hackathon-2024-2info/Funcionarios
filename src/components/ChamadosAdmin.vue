5fbe2d96c38afe710faaf95aecd10ef4758555d1
melloyasminn <melloyasminm@gmail.com>
2024-11-27 14:37:25 -0300

    feat: finalizado #103

--- src/components/ChamadosAdmin.vue
+++ src/components/ChamadosAdmin.vue
@@ -1,21 +1,30 @@
 <script setup>
 const itens = [
+{
+  id: '111',
+  status: 'andamento',
+  data:'11/11/11',
+  solicitador: 'Edson'
+},
+{
+  id: '111',
+  status: 'andamento',
+  data:'11/11/11',
+  solicitador: 'Edson'
+}
 ]
 </script>
 
 <template>
-  <main>
+
+  <div class="large">
+    <main>
     <div class="filtrar">
-      <button class="btn-azul">ADICIONAR ITEM AO ESTOQUE</button>
+
       <label for="">Filtrar:</label>
       <select name="Selecione" id="Selecione">
         <option value="" class="option">SELECIONE</option>
       </select>
-    </div>
-    <div class="setas">
-      <button><img src="../assets/esquerda.png" alt="" /></button>
-      <p>1</p>
-      <button><img src="../assets/direita.png" alt="" /></button>
     </div>
   </main>
 
@@ -27,7 +36,7 @@ const itens = [
             <section class="order-icons">
               <img src="../assets/Vector-1.png" alt="" /> <img src="../assets//Vector.png" alt="" />
             </section>
-            <span> Código do chamado </span>
+            <span> N° do chamado </span>
           </div>
         </th>
         <th>
@@ -35,7 +44,7 @@ const itens = [
           <section class="order-icons">
             <img src="../assets/Vector-1.png" alt="" /> <img src="../assets//Vector.png" alt="" />
           </section>
-          <span> Título do chamado </span>          
+          <span> Status do chamado </span>          
         </div>
         </th>
         <th>
@@ -43,7 +52,7 @@ const itens = [
           <section class="order-icons">
             <img src="../assets/Vector-1.png" alt="" /> <img src="../assets//Vector.png" alt="" />
           </section>
-          <span> Data do chamado </span>
+          <span> Data</span>
         </div>
         </th>
         <th>
@@ -51,7 +60,7 @@ const itens = [
           <section class="order-icons">
             <img src="../assets/Vector-1.png" alt="" /> <img src="../assets//Vector.png" alt="" />
           </section>
-          <span> Marca do item </span>
+          <span> Solicitador </span>
         </div>
         </th>
       </tr>
@@ -59,14 +68,63 @@ const itens = [
     <tbody>
       <tr v-for="item in itens" :key="item.id">
         <td>{{ item.id }}</td>
-        <td>{{ item.nome }}</td>
-        <td>{{ item.quantidade }}</td>
-        <td>{{ item.marca }}</td>
+        <td>{{ item.status }}</td>
+        <td>{{ item.data }}</td>
+        <td>{{ item.solicitador }}</td>
       </tr>
     </tbody>
 
   </table>
+  </div>
+  
+<div class="small">
+  <main>
+    <div class="filtrar">
 
+      <label for="">Filtrar:</label>
+      <select name="Selecione" id="Selecione">
+        <option value="" class="option">SELECIONE</option>
+      </select>
+    </div>
+  </main>
+
+  <table class="tabela-itens">
+    <thead>
+      <tr>
+        <th>
+          <div class="th-content">
+            <span> N° do chamado </span>
+          </div>
+        </th>
+        <th>
+          <div class="th-content">
+          <span> Status do chamado </span>          
+        </div>
+        </th>
+        <th>
+          <div class="th-content">
+          <span> Data</span>
+        </div>
+        </th>
+        <th>
+          <div class="th-content">
+          <span> Solicitador </span>
+        </div>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in itens" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.status }}</td>
+        <td>{{ item.data }}</td>
+        <td style="text-align: center;">{{ item.solicitador }}</td>
+        <button class="seta"><img src="../assets/Group 37329.png" alt=""></button>
+      </tr>
+    </tbody>
+
+  </table>
+</div>
 
 </template>
 
@@ -76,6 +134,13 @@ const itens = [
 body{
   color: #F5F5F5;
 }
+
+@media only screen and (min-device-width: 601px) {
+
+.small{
+  display: none;
+}
+
 .tabela {
   display: grid;
   grid-template-columns: 25% 25% 25% 25%;
@@ -102,18 +167,12 @@ body{
     display: flex;
     align-items: center; /* Alinha o conteúdo verticalmente */
     justify-content: flex-start; /* Se quiser espaçar igualmente */
+    
   }
 }
 
 span{
   margin-left: 5%;}
-
-/* .icons-and-text {
-  display: flex;
-  flex-direction: row;  
-  align-items: center;
-  gap: 2%;
-} */
 
 .order-icons {
   display: flex;
@@ -124,20 +183,6 @@ span{
     height: 8px;
   
   }
-}
-
-.btn-azul {
-  border: none;
-  border-radius: 5px;
-  color: white;
-  font-family: 'Karla';
-  background-color: #091d87;
-  padding: 1.5%;
-  padding-left: 5%;
-  padding-right: 5%;
-  font-weight: 700;
-  margin-left: 10%;
-  margin-right: 10%;
 }
 
 main {
@@ -231,7 +276,155 @@ li {
   margin-bottom: -10%;
 }
 
-/* img {
-  display:;
-} */
+}
+
+
+@media only screen and (max-device-width: 600px) {
+.seta{
+background: none;
+}
+.large{
+  display: none;
+}
+span{
+  font-weight: 500;
+  font-size: 15px;
+  width: 70%;
+  line-height: 90%;
+  text-align: left;
+  margin-bottom: 10%;
+  padding-top: 15%;
+}
+
+img {
+  margin-bottom: -30%;
+
+}
+.tabela {
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+  margin-left: 5%;
+  padding-right: 10%;
+}
+
+.tabela-itens {
+
+  font-family: 'Roboto', sans-serif;
+  width: 85%;
+  border-collapse: collapse;
+  margin: 0 auto;
+
+  line-height: 2.5rem;
+
+  & td {
+    border-bottom: 0.5px solid #d4d2d2;    
+   
+  }
+
+  & .th-content { 
+    display: flex;
+    align-items: center; /* Alinha o conteúdo verticalmente */
+    justify-content: flex-start; /* Se quiser espaçar igualmente */
+  }
+}
+
+
+
+.order-icons {
+  display: flex;
+  flex-direction: column;  
+
+  & img {
+    width: 8px;
+    height: 8px;
+  
+  }
+}
+
+main {
+  display: flex;
+  margin-bottom: 3%;
+
+  width: 100%;
+
+}
+
+
+label {
+  margin-right: 3%;
+}
+.filtrar {
+  flex-wrap: nowrap;
+  width: 100%;
+margin: 0 30% 0 30%;
+padding-top: 5%;
+
+}
+
+label,
+select,
+option {
+  font-family: 'Karla';
+}
+p {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  font-style: normal;
+  margin-left: 2%;
+  margin-right: 2%;
+  background: rgb(84, 111, 255, 0.32);
+  padding: 1.9%;
+  border-radius: 10px;
+}
+
+button {
+  border: none;
+  background: white;
+}
+
+select {
+  border-radius: 20px;
+  border: none;
+  background: rgb(84, 111, 255, 0.32);
+  color: #8787c7;
+  padding: 0.2%;
+  padding-left: 2%;
+  padding-right: 2%;
+}
+
+
+
+.option {
+  font-size: 17%;
+}
+
+ul,
+li {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+}
+li {
+  list-style: none;
+  padding: 5%;
+  margin-bottom: 5%;
+}
+.filtrar,
+.setas {
+  margin-top: 1%;
+}
+
+
+
+.linha {
+  width: 150%;
+  margin-top: 5%;
+  margin-bottom: -10%;
+}
+#ultimalinha {
+  width: 100%;
+  margin-top: 5%;
+  margin-bottom: -10%;
+}
+
+}
 </style>
