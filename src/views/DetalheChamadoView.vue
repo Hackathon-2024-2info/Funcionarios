06526cf744b7193ace114d7fab3e0f339991a959
eduardafh <eduardafheidemann@gmail.com>
2024-11-29 08:58:38 -0300

    feat: mobile finalizado #102

---
+++ src/views/DetalheChamadoView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MenuFuncionarios from "@/components/MenuFuncionarios.vue";
+import pagHeader from "@/components/pagHeader.vue";
+import DetalheChamado from "@/components/DetalheChamado.vue";
+</script>
+
+<template> 
+<pagHeader></pagHeader>
+<MenuFuncionarios></MenuFuncionarios>
+<DetalheChamado></DetalheChamado>
+</template>
