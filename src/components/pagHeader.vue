bc48ba09118549b083994e541d58f0729e4d694b
Nicole Alves Brand <131276280+nicoleabrand@users.noreply.github.com>
2024-12-05 10:15:32 -0300

    Revert "Feat 131"

--- src/components/pagHeader.vue
+++ src/components/pagHeader.vue
@@ -1,134 +1,131 @@
 <script setup>
-// import { useAuthStore } from '@/stores/auth';
-// const authStore = useAuthStore()
+
 </script>
 
+
 <template>
-  <main>
+    <main>
     <header>
-      <h1 class="block-effect" style="--td: 0.9s">
-        <div class="block-reveal" style="--bc: rgba(217, 217, 217, 1); --d: 0.25s">Olá, Edson!</div>
-      </h1>
-      <RouterLink to="/perfil"><img src="../assets/foto-perfil.png" alt="" /></RouterLink>
-      <RouterLink to="/"><img class="logout" src="../assets/logout.webp" alt="" /></RouterLink>
+        <h1 class="block-effect" style="--td: 0.9s">
+  <div class="block-reveal" style="--bc: rgba(217, 217, 217, 1); --d: 0.25s">Olá, Edson!</div>
+</h1>
+        <RouterLink to="/perfil"><img src="../assets/foto-perfil.png" alt=""></RouterLink>
     </header>
-  </main>
+</main>
 </template>
 
 <style scoped>
-@media only screen and (min-device-width: 601px) {
-  .logout {
-    display: flex;
-    position: absolute;
-    margin-left: 94%;
-    top: calc(55% - 43px / 2 - 0.5px);
-    width: 40px;
+@media only screen and (min-device-width: 601px){
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  min-height: 100vh;
+  padding: 20px;
+}
+
+
+/* other */
+.info {
+  margin: 20px 0;
+  text-align: center;
+}
+
+
+/* block-$ */
+.block-effect {
+  font-size: calc(8px + 2.5vw);
+}
+
+.block-reveal {
+  --t: calc(var(--td) + var(--d));
+
+  color: transparent;
+  padding: 4px;
+
+  position: relative;
+  overflow: hidden;
+
+  animation: revealBlock 1s var(--t) forwards;
+  border-radius: 5px 5px 5px 5px;
+}
+
+.block-reveal::after {
+  content: '';
+
+  width: 0%;
+  height: 100%;
+  padding-bottom: 4px;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  background: var(--bc);
+  animation: revealingIn var(--td) var(--d) forwards, revealingOut var(--td) var(--t) forwards;
+}
+
+
+/* animations */
+@keyframes revealBlock {
+  100% {
+    color: #0f0f0f;
+  }
+}
+
+@keyframes revealingIn {
+
+  0% {
+    width: 0;
+    opacity: 100%;
   }
 
-  *,
-  *::before,
-  *::after {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  100% {
+    width: 100%;
+    opacity: 100%;
+  }
+}
+
+@keyframes revealingOut {
+
+  0% {
+    transform: translateX(0);
   }
 
-  body {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    min-height: 100vh;
-    padding: 20px;
+  100% {
+    transform: translateX(100%);
   }
 
-  /* other */
-  .info {
-    margin: 20px 0;
-    text-align: center;
-  }
+}
 
-  /* block-$ */
-  .block-effect {
-    font-size: calc(8px + 2.5vw);
-  }
 
-  .block-reveal {
-    --t: calc(var(--td) + var(--d));
-
-    color: transparent;
-    padding: 4px;
-
-    position: relative;
-    overflow: hidden;
-
-    animation: revealBlock 1s var(--t) forwards;
-    border-radius: 5px 5px 5px 5px;
-  }
-
-  .block-reveal::after {
-    content: '';
-
-    width: 0%;
-    height: 100%;
-    padding-bottom: 4px;
-
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    background: var(--bc);
-    animation:
-      revealingIn var(--td) var(--d) forwards,
-      revealingOut var(--td) var(--t) forwards;
-  }
-
-  /* animations */
-  @keyframes revealBlock {
-    100% {
-      color: #0f0f0f;
-    }
-  }
-
-  @keyframes revealingIn {
-    0% {
-      width: 0;
-      opacity: 100%;
-    }
-
-    100% {
-      width: 100%;
-      opacity: 100%;
-    }
-  }
-
-  @keyframes revealingOut {
-    0% {
-      transform: translateX(0);
-    }
-
-    100% {
-      transform: translateX(100%);
-    }
-  }
-
-  main {
+main{
     padding-bottom: 10%;
-  }
+}
 
-  header {
+header {
     display: flex;
     margin-left: 18%;
     position: absolute;
     width: 82%;
     height: 18vh;
-    background-color: #d9d9d9;
-  }
+    background-color: #D9D9D9;
+}
 
-  h1 {
+h1 {
     position: absolute;
     left: 74px;
-    top: calc(50% - 43px / 2 - 0.5px);
+    top: calc(50% - 43px/2 - 0.5px);
     font-family: 'Readex Pro';
     font-style: normal;
     font-weight: 600;
@@ -139,24 +136,24 @@
     text-align: center;
     letter-spacing: 0.01em;
     color: #000000;
-  }
+}
 
-  img {
+img {
     position: absolute;
     display: flex;
     margin-left: 88%;
-    top: calc(50% - 43px / 2 - 0.5px);
-  }
-  img:hover {
+    top: calc(50% - 43px/2 - 0.5px);
+}
+  img:hover{
     scale: 1.05;
-    border-radius: 60%;
+    border-radius: 60%;    
     background-color: rgb(99, 99, 245);
   }
 }
 
-@media only screen and (max-device-width: 600px) {
-  * {
-    display: none;
-  }
+@media only screen and (max-device-width: 600px){
+    *{
+        display: none;
+    }
 }
 </style>
